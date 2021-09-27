340e50a68618c3b8c0aa2853ea90d9eda67d143e
utouto97 <utouto97@gmail.com>
2021-09-27 21:54:21 +0900

    add room list to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-full">
+    <h2 class="mb-2 text-lg font-bold">ルーム一覧</h2>
+    <div v-for="room in rooms" :key="room.id">
+      <router-link v-bind:to="{ name: 'Room', params: { id: room.uid } }" class="py-4 w-full block border text-lg hover:bg-gray-200">
+        {{ room.name }}
+      </router-link>
+    </div>
     <Navbar />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent, ref, onMounted } from "vue";
+import useFirebaseAuth from "@/common/auth";
+import getApi from "@/common/api";
 import Navbar from "@/components/Navbar";
 
-export default {
-  name: 'Home',
+export default defineComponent({
   components: {
-    HelloWorld,
     Navbar
   },
-  methods: {
-  }
-}
+  setup() {
+    const { token } = useFirebaseAuth();
+
+    const rooms = ref([]);
+
+    onMounted(async () => {
+      const res = await getApi(token.value).get("/api/v1/user/room");
+      rooms.value = res.data.rooms;
+    });
+
+    return {
+      rooms
+    };
+  },
+});
 </script>
