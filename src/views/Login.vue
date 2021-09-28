18c5c0ae0e2acc59f02f2bed8df24197778c9ba4
utouto97 <utouto97@gmail.com>
2021-09-28 13:01:25 +0900

    add error message into Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,9 +5,12 @@
         <label for="email" class="block text-gray-700 text-sm font-bold mb-2">メールアドレス</label>
         <input type="email" v-model="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
       </div>
-      <div class="mb-6">
+      <div class="mb-4">
         <label for="password" class="block text-gray-700 text-sm font-bold mb-2">パスワード</label>
         <input type="password" v-model="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+      </div>
+      <div class="mb-4 text-sm text-red-500">
+        {{ error }}
       </div>
       <div class="flex items-center justify-between">
         <router-link to="/register" type="submit" class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">新規登録</router-link>
@@ -28,17 +31,24 @@ export default defineComponent({
 
     const email = ref("");
     const password = ref("");
+    const error = ref("");
 
     const { login } = useFirebaseAuth();
 
     const doLogin = async () => {
-      await login(email.value, password.value);
+      try {
+        await login(email.value, password.value);
+      } catch(e) {
+        error.value = "ログインに失敗しました。";
+      }
+
       router.push("/");
     };
 
     return {
       email,
       password,
+      error,
       doLogin,
     };
   },
